f0110e9bcd4ed1657c9bd405df90a9889106aa46
Luis Jackniel Arellano <rayreckl.j.a.s01@gmail.com>
2023-03-09 22:39:34 -0700

    Se modifico la introduccion
    Se agrego audio a la introduccion

--- scripts/Intro.gd
+++ scripts/Intro.gd
@@ -1,7 +1,7 @@
 extends Control
 
-func _on_LogoAnimado_finished():
+func _on_SaltarIntro_pressed():
 	get_tree().change_scene("res://Esenas/MenuP1.tscn")
 
-func _on_SaltarIntro_pressed():
+func _on_AnimationPlayer_animation_finished(anim_name):
 	get_tree().change_scene("res://Esenas/MenuP1.tscn")
