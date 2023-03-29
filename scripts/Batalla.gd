be9dabb99f128692b4dd274b078a4e2b18abebd7
Luis Jackniel Arellano <rayreckl.j.a.s01@gmail.com>
2023-03-28 17:24:31 -0700

    Se modificaron diseños
    Se modifico el diseño de la barra de vida de Salter y el jefe

--- scripts/Batalla.gd
+++ scripts/Batalla.gd
@@ -1,7 +1,5 @@
 extends Control
 
-
-
 func _ready():
 	get_node("Personajes/Vida Jefe").max_value = Globales.VidaJefe
 	get_node("Personajes/EstaminaJefe").max_value = Globales.EstaminaJefe
@@ -22,43 +20,8 @@ func _process(delta):
 	#Label Turno
 	get_node("Acciones/Turno").text = str("Turno: " ) + str(Globales.Turno)
 
-func _on_Ataque1_pressed():
-	SubfusilJefe()
-
-func _on_Vida_pressed():
-	SuministrosJefe()
-
-func _on_Salir_pressed():
-	get_tree().quit()
-
 func _on_RegresarMenu_pressed():
 	get_tree().change_scene("res://Esenas/MenuP1.tscn")
 
-#Funciones de Evaluacion
-
-func EvaluarEstamina(var Estamina, var EstaminaMaxima):
-	if Estamina > EstaminaMaxima:
-		return EstaminaMaxima
-	else:
-		return Estamina
-
-func EvaluarVida(var vida, var vidaMaxima):
-	if	vida > vidaMaxima:
-		return vidaMaxima
-	else:
-		return vida
-
-#Funciones de ataque
-func SubfusilJefe():
-	if Globales.EstaminaJefe >= 10:
-		Globales.VidaSalter = Globales.VidaSalter - (Globales.Ataquejefe - (Globales.DefensaSalter * (Globales.Ataquejefe/100.00)))
-		Globales.EstaminaJefe = Globales.EstaminaJefe - 10
-		Globales.VidaJefe = Globales.VidaJefe - (Globales.AtaqueSalter - (Globales.DefensaJefe * (Globales.AtaqueSalter/100.00)))
-	Globales.Turno = Globales.Turno + 1
-
-func SuministrosJefe():
-	Globales.VidaJefe = Globales.VidaJefe + (Globales.VidaMaximaJefe * 0.10)
-	Globales.VidaJefe = EvaluarVida(Globales.VidaJefe, Globales.VidaMaximaJefe)
-	Globales.EstaminaJefe = Globales.EstaminaJefe + (Globales.EstaminaMaximaJefe * 0.10)
-	Globales.EstaminaJefe = EvaluarEstamina(Globales.EstaminaJefe, Globales.EstaminaMaximaJefe)
-	Globales.Turno = Globales.Turno + 1
+func _on_Salir_pressed():
+	get_tree().quit()
--- scripts/Globales.gd
+++ scripts/Globales.gd
@@ -6,27 +6,27 @@ var max_loadtime = 100000
 
 #Estadisticas Jefe
 
-var VidaJefe = 100.00
+var VidaJefe = 1000.00
 var DefensaJefe = 50.00
-var Ataquejefe = 10.00
-var EspecialJefe = 0.00
-var DefEspJefe = 0.00
-var VelocidadJefe = 0.00
+var Ataquejefe = 125.00
+var EspecialJefe = 32.00
+var DefEspJefe = 30.00
+var VelocidadJefe = 70.00
 var EstaminaJefe = 100.00
 #Valores maximos
 
-var VidaMaximaJefe = 100.00
+var VidaMaximaJefe = 1000.00
 var EstaminaMaximaJefe = 100.00
 #Estadisticas Salter
 
-var VidaSalter = 200.00
-var DefensaSalter = 0.00
-var AtaqueSalter = 10.00
+var VidaSalter = 3000.00
+var DefensaSalter = 50.00
+var AtaqueSalter = 84.00
 var EspecialSalter = 0.00
-var DefEspSalter = 0.00
-var VelocidadSalter = 0.00
-var EstaminaSalter = 0.00
-var VidaMaximaSalter = 0.00
+var DefEspSalter = 70.00
+var VelocidadSalter = 70.00
+var EstaminaSalter = 500.00
+var VidaMaximaSalter = 3000.00
 #Valores maximos
 
 #Turno
