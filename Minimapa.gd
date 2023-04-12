006c44ccd7fe243426852c5dd5c39c853183671a
crybaby99 <110852456+crybaby99@users.noreply.github.com>
2023-04-11 21:03:20 -0700

    CABRON TA CHIDO EL MINIMAPA
    SE CREO EL MINIMAPA PARA EL MENU DE CONFIGURACION.

--- Minimapa.gd
+++ Minimapa.gd
@@ -1,16 +1,27 @@
 extends MarginContainer
 
-
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
+export (NodePath) var player
+export var zoom= 1.5
 
+onready var grid = $MarginContainer/Grid
+onready var player_m = $MarginContainer/Grid/PlayerMark
+
+var grid_scale
+var markers = {}
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	player_m.position = grid.rect_size / 2
+	grid_scale = grid.rect_size / (get_viewport_rect().size * zoom)
+#	pass # Replace with function body.
 
-
+func _process (delta):
+	if ! player:
+		return
+	player_m.rotation = get_node(player).rotation + PI / 2
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
--- Sprite/Jugador.gd
+++ Sprite/Jugador.gd
@@ -50,13 +50,3 @@ func _physics_process(delta):
 
 
 
-
-
-func _on_Colision_pelican_body_entered(body):
-	var layer_to_hide = get_node("/root/mapa/Mapa/cesped/Pelican/pelican abajo")
-	layer_to_hide.set_visible(false)
-
-
-func _on_Colision_pelican_body_exited(body):
-	var layer_to_hide = get_node("/root/mapa/Mapa/cesped/Pelican/pelican abajo")
-	layer_to_hide.set_visible(true)
