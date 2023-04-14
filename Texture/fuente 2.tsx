d8b3b09f86f8aaa7142e3b8c9f88ea0d2c473e16
mctison12 <123214801+mctison12@users.noreply.github.com>
2023-04-13 23:04:29 -0700

    se cambio el mapa principal y escenas

---
+++ Texture/fuente 2.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.1" name="fuente 2" tilewidth="32" tileheight="32" tilecount="2457" columns="273">
+ <image source="png/altar 224x288 - standing on grass.png" width="8736" height="288"/>
+</tileset>
--- escenas/Minimapa.gd
+++ escenas/Minimapa.gd
@@ -1,11 +1,18 @@
 extends Control
 
-var test = preload("res://scripts/mapa principal.gd")
 
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
 func _ready():
+	 # Replace with function body.
 	$MarginContainer/TextureRect/ViewportContainer/Viewport/mapa/Jugador/Camera2D.zoom=Vector2(1,1)
-	
-	Globales.POx = $MarginContainer/TextureRect/ViewportContainer/Viewport/mapa/Jugador.position.x
-	Globales.POy = $MarginContainer/TextureRect/ViewportContainer/Viewport/mapa/Jugador.position.y
-	
-	print(Globales.POx," : ",Globales.POy)
+#	$MarginContainer/TextureRect/ViewportContainer/Viewport/Jugador/Camera2D.zoom=Vector2(8,8)
+#	$ViewportContainer/Viewport/Jugador/Camera2D.zoom=Vector2(3,3)
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
