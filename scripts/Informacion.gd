0e7afac53cf69b16acd03809ae17c8408ef3b169
Luis Jackniel Arellano <rayreckl.j.a.s01@gmail.com>
2023-04-17 03:33:29 -0700

    Bug fix

--- scripts/Informacion.gd
+++ scripts/Informacion.gd
@@ -4,16 +4,19 @@ func _ready():
 	verifyHealth()
 
 func verifyHealth():
-	if Globales.VidaJefe <= 0:
-		$Muerte.visible = true
-		$Victoria.visible = false
-	else:
-		$Muerte.visible = false
-		$Victoria.visible = true
 	if Globales.VidaJefe and Globales.VidaSalter <= 0:
 		$Muerte.visible = false
 		$Victoria.visible = false
 		$Empate.visible = true
+	if Globales.VidaJefe <= 0:
+		$Muerte.visible = true
+		$Victoria.visible = false
+		$Empate.visible = false
+	else:
+		$Muerte.visible = false
+		$Victoria.visible = true
+		$Empate.visible = false
+	
 
 func _on_Regresar_pressed():
 	ControlsFmod.playEvent("Button")
